f5853e9b98dba7399852db9b1d8084ad58aa361c
Antuan24 <antoniomaciaszambrano24@gmail.com>
2020-03-30 12:41:44 +0200

    [New] Formulario edición usuarios

--- board/user/views.py
+++ board/user/views.py
@@ -3,11 +3,11 @@ from django.contrib.auth import logout as do_logout
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as do_login
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import check_password, make_password
 from django import forms
 
 from user.models import User
-from user.forms import NewUser, Register
+from user.forms import NewUser, Register, editAccount, editProfile
 from reviews.models import Valoration, Comment
 
 
@@ -56,28 +56,6 @@ def delete_myUSer(request, pk):
   
     
     return redirect('/')
-def edit_user(request, pk):
-    # Recuperamos la instancia de la persona
-    instancia = User.objects.get(id=pk)
-
-    # Creamos el formulario con los datos de la instancia
-    form = NewUser(instance=instancia)
-
-    # Comprobamos si se ha enviado el formulario
-    if request.method == "POST":
-        # Actualizamos el formulario con los datos recibidos
-        form = NewUser(request.POST, instance=instancia)
-        # Si el formulario es válido...
-        if form.is_valid():
-            # Guardamos el formulario pero sin confirmarlo,
-            # así conseguiremos una instancia para manejarla
-            instancia = form.save(commit=False)
-            # Podemos guardarla cuando queramos
-            instancia.save()
-            return  redirect('/profile/{}'.format(pk))
-
-    # Si llegamos al final renderizamos el formulario
-    return render(request, "newuser.html", {'form': form})
 
 def new_user(request):
     if(request.method=='POST'):
@@ -101,4 +79,67 @@ def new_user(request):
             return redirect('/profile/{}'.format(user.id))
     else:
         formulario = Register()
+    return render(request,"newuser.html",{"form":formulario})
+
+def edit_account(request):
+    
+    if(request.method=='POST'):
+        formulario = editAccount(request.POST)
+
+
+        if(formulario.is_valid()):
+
+            username = request.user.username
+            password1 = formulario.cleaned_data['password3']
+            user1 = authenticate(username=username, password=password1)
+            
+            if user1 is None:
+                formulario.add_error('password3','Contraseña incorrecta')
+
+            if(formulario.cleaned_data['password1']!=formulario.cleaned_data['password2']):
+                formulario.add_error('password2','No coinciden las contraseñas')
+            
+            if(formulario.errors):
+                return render(request,"newuser.html",{"form":formulario})
+
+            username = formulario.cleaned_data['username']
+            password = make_password(formulario.cleaned_data['password1'])
+
+            User.objects.filter(id=request.user.id).update(username=username, password=password)
+            user = get_object_or_404(User,pk=request.user.id)
+
+            do_logout(request)
+            do_login(request, user)
+
+            return redirect('/profile/{}'.format(user.id))
+
+    else:
+        formulario = editAccount()
+
+    return render(request,"newuser.html",{"form":formulario})
+
+def edit_profile(request):
+    
+    if(request.method=='POST'):
+        formulario = editProfile(request.POST)
+
+
+        if(formulario.is_valid()):
+            
+            if(formulario.errors):
+                return render(request,"newuser.html",{"form":formulario})
+
+            name = formulario.cleaned_data['name']
+            last_name = formulario.cleaned_data['last_name']
+            email = formulario.cleaned_data['email']
+            bio = formulario.cleaned_data['bio']
+            picture = formulario.cleaned_data['picture']
+
+            User.objects.filter(id=request.user.id).update(first_name=name,last_name=last_name,email=email,bio=bio,picture=picture)
+
+            return redirect('/profile/{}'.format(request.user.id))
+
+    else:
+        formulario = editProfile()
+
     return render(request,"newuser.html",{"form":formulario})
