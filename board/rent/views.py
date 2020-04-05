f154f8b4929296edfb6892190835bb1da2d5da6c
Adrrolroj <adrrolroj@alum.us.es>
2020-04-05 12:23:03 +0200

    [New] Add localization to games

--- board/rent/views.py
+++ board/rent/views.py
@@ -1,5 +1,8 @@
+import logging
 import string
 import random
+import requests
+
 from datetime import date, datetime
 
 from django.shortcuts import render ,  get_object_or_404
@@ -55,8 +58,12 @@ def rents_list(request):
 
 def games_detail(request,pk):
      dato = get_object_or_404(Game, pk=pk)
+     ubicacion = dato.address
+     response = requests.get('https://eu1.locationiq.com/v1/search.php?key=pk.bfdfa73760621b89cf9e8435ffcf48df&q=' + ubicacion + '&format=json')
+     geodata = response.json()
+
      return render(request,'gameDetail.html', {'name':dato.name, 'description':dato.description,'price': dato.price ,
-      'status': dato.status,'picture' : dato.picture, 'id' : dato.id,'owner': dato.owner })
+      'status': dato.status,'picture' : dato.picture, 'id' : dato.id,'owner': dato.owner,'longitude': geodata[0]['lon'], 'latitude': geodata[0]['lat']})
 
 def delete(request, pk):
     # Recuperamos la instancia de la persona y la borramos
