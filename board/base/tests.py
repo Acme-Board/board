492a854a7010d46153082aef39a440ef384c34c4
juaparser <juaparser@alum.us.es>
2020-04-16 18:44:41 +0200

    [Add] Base selenium tests

--- board/base/tests.py
+++ board/base/tests.py
@@ -9,7 +9,7 @@ import unittest, time, re
 
 class Index(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
         self.base_url = "https://www.google.com/"
         self.verificationErrors = []
@@ -49,7 +49,7 @@ class Index(unittest.TestCase):
 
 class Login(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
         self.base_url = "https://www.google.com/"
         self.verificationErrors = []
@@ -90,9 +90,8 @@ class Login(unittest.TestCase):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
 
-class Register(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
         self.base_url = "https://www.google.com/"
         self.verificationErrors = []
@@ -104,23 +103,20 @@ class Register(unittest.TestCase):
         driver.find_element_by_xpath("//div[@id='navbarSupportedContent']/ul/li[3]/a/b").click()
         driver.find_element_by_id("id_username").click()
         driver.find_element_by_id("id_username").clear()
-        driver.find_element_by_id("id_username").send_keys("Beatriz")
+        driver.find_element_by_id("id_username").send_keys("Lucia")
         driver.find_element_by_id("id_password1").clear()
         driver.find_element_by_id("id_password1").send_keys("1234567890")
         driver.find_element_by_id("id_password2").clear()
         driver.find_element_by_id("id_password2").send_keys("1234567890")
-        driver.find_element_by_id("id_name").click()
         driver.find_element_by_id("id_name").clear()
-        driver.find_element_by_id("id_name").send_keys("Beatriz")
+        driver.find_element_by_id("id_name").send_keys("Lucia")
         driver.find_element_by_id("id_last_name").clear()
-        driver.find_element_by_id("id_last_name").send_keys("Tricia")
+        driver.find_element_by_id("id_last_name").send_keys("Manuel")
         driver.find_element_by_id("id_email").clear()
-        driver.find_element_by_id("id_email").send_keys("bea@gmail.com")
+        driver.find_element_by_id("id_email").send_keys("lucia@gmail.com")
         driver.find_element_by_id("id_bio").clear()
-        driver.find_element_by_id("id_bio").send_keys("Hola, soy bea.")
-        driver.find_element_by_id("id_picture").click()
-        driver.find_element_by_id("id_picture").clear()
-        driver.find_element_by_id("id_picture").send_keys("C:\\fakepath\\DyxmCipX0Ac05NG.jpg")
+        driver.find_element_by_id("id_bio").send_keys("Soy lucia")
+        driver.find_element_by_xpath("//tr[8]/td").click()
         driver.find_element_by_id("id_check").click()
         driver.find_element_by_xpath("//button[@type='submit']").click()
     
